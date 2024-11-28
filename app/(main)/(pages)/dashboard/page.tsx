57342778fbd7d5f75097c1f70362de5433b526fb
Parth Doshi <parthdoshi480@gmail.com>
2024-11-28 12:58:23 +0530

    Protected Routes added

--- app/(main)/(pages)/dashboard/page.tsx
+++ app/(main)/(pages)/dashboard/page.tsx
@@ -4,7 +4,7 @@ const page = () => {
   return (
       <div 
       className="text-gray-600 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white px-3 py-2 rounded-md text-4xl font-medium transition-colors duration-200"
-    >Welcome To Dashboard</div>
+    >Welcome To Dashboards...</div>
   )
 }
 
