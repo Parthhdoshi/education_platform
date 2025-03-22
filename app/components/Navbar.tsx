57a1f8c8f87f9acf598d10568fadb49e2a49687c
Manan1884 <manan18doshi@gmail.com>
2025-03-22 22:10:54 +0530

    Footer.tsx and Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,10 @@
 "use client";{/*started by sunny*/}
-
 import { useState } from "react";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <nav className="bg-[#BFDBFE] py-4 px-6">
+    <nav className="bg-[#daf0ff] py-4 px-6">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
         {/* Logo */}
         <div className="text-blue-600 font-semibold text-lg md:text-xl">
@@ -25,50 +24,27 @@ const Navbar = () => {
             </a>
           </li>
           <li>
-<<<<<<< HEAD
             <a href="/courses" className="hover:text-blue-600 transition duration-300">
-=======
-            <a href="/course" className="hover:text-blue-600 transition duration-300">
->>>>>>> 2b7091864aada4f1dcbc3e78d561e1e5b415e4e3
               Courses
             </a>
           </li>
-           {/* Mahesh start */}
           <li>
-<<<<<<< HEAD
-<<<<<<< HEAD
             <a href="/contactus" className="hover:text-blue-600 transition duration-300">
-=======
-            <a href="/aboutus" className="hover:text-blue-600 transition duration-300">
->>>>>>> 6cea5ae56d2b9256c147b16792d07250fe84fa08
-=======
-            <a href="/Contactus" className="hover:text-blue-600 transition duration-300">
->>>>>>> 2b7091864aada4f1dcbc3e78d561e1e5b415e4e3
               Contact Us
             </a>
           </li>
-          {/* Mahesh end */}
         </ul>
-      
 
         {/* Buttons */}
         <div className="hidden md:flex space-x-4 items-center">
           <a
-<<<<<<< HEAD
             href="http://localhost:3000/signin#signup"
-=======
-            href="/signin#signup"
->>>>>>> 6cea5ae56d2b9256c147b16792d07250fe84fa08
             className="text-blue-600 font-medium hover:text-blue-800 transition duration-300"
           >
             Sign up
           </a>
           <a
-<<<<<<< HEAD
             href="http://localhost:3000/signin"
-=======
-            href="/signin#signin"
->>>>>>> 6cea5ae56d2b9256c147b16792d07250fe84fa08
             className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition duration-300"
           >
             Log in
@@ -126,7 +102,7 @@ const Navbar = () => {
               <ul className="space-y-4 text-gray-700 font-medium">
                 <li>
                   <a
-                    href="/"
+                    href="#"
                     className="hover:text-blue-600 transition duration-300"
                   >
                     Home
@@ -134,11 +110,7 @@ const Navbar = () => {
                 </li>
                 <li>
                   <a
-<<<<<<< HEAD
                     href="/aboutus"
-=======
-                    href="/about"
->>>>>>> 6cea5ae56d2b9256c147b16792d07250fe84fa08
                     className="hover:text-blue-600 transition duration-300"
                   >
                     About
@@ -154,15 +126,7 @@ const Navbar = () => {
                 </li>
                 <li>
                   <a
-<<<<<<< HEAD
-<<<<<<< HEAD
                     href="contactus"
-=======
-                    href="/contactus"
->>>>>>> 6cea5ae56d2b9256c147b16792d07250fe84fa08
-=======
-                    href="/Contactus"
->>>>>>> 2b7091864aada4f1dcbc3e78d561e1e5b415e4e3
                     className="hover:text-blue-600 transition duration-300"
                   >
                     Contact Us
