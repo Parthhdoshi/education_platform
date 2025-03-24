f19d23d76985bad8b5c97ad348acbff096a671eb
Sandipbaranwal <sandipbaranwal60@gmail.com>
2025-03-24 19:18:43 +0530

    Resolved Merged Conflicts

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -30,11 +30,7 @@ const Navbar = () => {
             </a>
           </li>
           <li>
-<<<<<<< HEAD
             <a href="/contactus" className="hover:text-blue-600 transition duration-300">
-=======
-            <a href="/aboutus" className="hover:text-blue-600 transition duration-300">
->>>>>>> 6cea5ae56d2b9256c147b16792d07250fe84fa08
               Contact Us
             </a>
           </li>
@@ -43,21 +39,13 @@ const Navbar = () => {
         {/* Buttons */}
         <div className="hidden md:flex space-x-4 items-center">
           <a
-<<<<<<< HEAD
             href="http://localhost:3000/signin#signup"
-=======
-            href="/signin#signup"
->>>>>>> 6cea5ae56d2b9256c147b16792d07250fe84fa08
             className="text-blue-600 font-medium hover:text-blue-800 transition duration-300"
           >
             Sign up
           </a>
           <a
-<<<<<<< HEAD
             href="http://localhost:3000/signin"
-=======
-            href="/signin#signin"
->>>>>>> 6cea5ae56d2b9256c147b16792d07250fe84fa08
             className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition duration-300"
           >
             Log in
@@ -123,11 +111,7 @@ const Navbar = () => {
                 </li>
                 <li>
                   <a
-<<<<<<< HEAD
                     href="/aboutus"
-=======
-                    href="/about"
->>>>>>> 6cea5ae56d2b9256c147b16792d07250fe84fa08
                     className="hover:text-blue-600 transition duration-300"
                   >
                     About
@@ -143,11 +127,7 @@ const Navbar = () => {
                 </li>
                 <li>
                   <a
-<<<<<<< HEAD
                     href="contactus"
-=======
-                    href="/contactus"
->>>>>>> 6cea5ae56d2b9256c147b16792d07250fe84fa08
                     className="hover:text-blue-600 transition duration-300"
                   >
                     Contact Us
--- app/components/api/page.tsx
+++ app/components/api/page.tsx
@@ -1,16 +1,16 @@
-// "use client";
+"use client"; //made by Sandip
 
-// import React from "react";
-// import DogImage from "./dog/page";
-// import CarImage from "./image/page";
+import React from "react";
+import DogImage from "./dog/page";
+import CarImage from "./image/page";
 
-// const Page: React.FC = () => {
-//   return (
-//     <div className="p-32 flex gap-40">
-//       <DogImage />
-//       <CarImage />
-//     </div>
-//   );
-// };
+const Page: React.FC = () => {
+  return (
+    <div className="p-32 flex gap-40">
+      <DogImage />
+      <CarImage />
+    </div>
+  );
+};
 
-// export default Page;
+export default Page;
