427907f72e5e561e2200291d7b76d945dedba9c0
sadashiv zore <sadashivzore>
2025-04-05 14:57:22 +0530

    "Resolve next.config.ts"

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,42 @@
-import type { NextConfig } from "next";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['via.placeholder.com'], // allow the placeholder domain
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'media.istockphoto.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn4.vectorstock.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pinimg.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'c8.alamy.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.shutterstock.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'th.bing.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'motionarray.imgix.net',
+      },
+    ],
   },
-};
+}
 
-export default nextConfig;
+module.exports = nextConfig
+
